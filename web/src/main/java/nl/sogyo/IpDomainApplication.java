b0bd6bb30b25f69d8fc5eecb021a3e847ecd88d6
Kevin Koerts <kkoerts@sogyo.nl>
2025-12-17 16:12:50 +0100

    added more modules

---
+++ web/src/main/java/nl/sogyo/IpDomainApplication.java
@@ -0,0 +1,12 @@
+package nl.sogyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpDomainApplication.class, args);
+    }
+}
