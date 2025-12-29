48d16fa5c68dd11d3002988a35f9dd1fdd6b4a5f
Kevin Koerts <kkoerts@sogyo.nl>
2025-12-29 13:50:43 +0100

    added working greeting

--- persistence/src/main/java/nl/sogyo/ip/persistence/InMemoryIdentityRepository.java
+++ persistence/src/main/java/nl/sogyo/ip/persistence/InMemoryIdentityRepository.java
@@ -1,7 +1,6 @@
 package nl.sogyo.ip.persistence;
 
-import nl.sogyo.ip.interfaces.IdentityRepository;
-import nl.sogyo.ip.
+import  nl.sogyo.ip.interfaces.IdentityRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
